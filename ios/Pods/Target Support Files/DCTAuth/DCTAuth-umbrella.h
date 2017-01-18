a9c58dd77bdb671b24d1de0bbb58a534e27d754e
Adriano Tadao <drianotadao@gmail.com>
2017-01-17 22:57:37 -0200

    I'm addeding the oauth

---
+++ ios/Pods/Target Support Files/DCTAuth/DCTAuth-umbrella.h
@@ -0,0 +1,15 @@
+#import <UIKit/UIKit.h>
+
+#import "DCTAuth.h"
+#import "DCTAuthAccount.h"
+#import "DCTAuthAccountCredential.h"
+#import "DCTAuthAccountStore.h"
+#import "DCTAuthAccountSubclass.h"
+#import "DCTAuthPlatform.h"
+#import "DCTAuthRequest.h"
+#import "DCTAuthRequestMethod.h"
+#import "DCTAuthResponse.h"
+
+FOUNDATION_EXPORT double DCTAuthVersionNumber;
+FOUNDATION_EXPORT const unsigned char DCTAuthVersionString[];
+
